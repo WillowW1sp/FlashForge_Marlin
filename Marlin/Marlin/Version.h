6a09fabcbbefbb2b8e243a97a395c0a873d8fceb
WillowWisp <zcrawshaw@gmail.com>
2024-11-19 11:05:19 -0800

    Update Version.h

--- Marlin/Marlin/Version.h
+++ Marlin/Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-//#define SHORT_BUILD_VERSION "2.0.9.7"
+//#define SHORT_BUILD_VERSION "2.0.9.8"
 
 /**
  * Verbose version identifier which should contain a reference to the location
